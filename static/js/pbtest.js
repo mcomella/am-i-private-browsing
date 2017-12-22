f5ca0564e4df4f217b62724fd1453ea8a88ba87c
Michael Comella <michael.l.comella@gmail.com>
2017-12-21 20:55:18 -0800

    Move tests into array.

--- static/js/pbtest.js
+++ static/js/pbtest.js
@@ -4,30 +4,6 @@ var _TEST_VALUE = 'apparently not';
 var _CLASS_PASS = "pass";
 var _CLASS_FAIL = "fail";
 
-var _localStorage = {
-    id: "localStorage",
-    isSupported: !!window.localStorage,
-    init: function() { window.localStorage.setItem(_TEST_KEY, _TEST_VALUE); },
-    isFailed: function() { return window.localStorage.getItem(_TEST_KEY) !== null; },
-};
-
-var _sessionStorage = {
-    id: "sessionStorage",
-    isSupported: !!window.sessionStorage,
-    init: function() { window.sessionStorage.setItem(_TEST_KEY, _TEST_VALUE); },
-    isFailed: function() { return window.sessionStorage.getItem(_TEST_KEY) !== null; },
-};
-
-var _indexedDB = {
-    id: 'indexedDB',
-    isSupported: !!window.indexedDB,
-
-    _open: function() { return window.indexedDB.open('PB', 1); },
-    //.onsuccess .onerror .onupgradeneeded
-    init: function() { },
-    isFailed: function() { },
-};
-
 function updatePass(id) {
     document.getElementById(id).className = _CLASS_PASS;
 }
@@ -48,11 +24,27 @@ function runTest(test) {
     test.init(); // for page reload.
 }
 
-var _tests = [
-    _localStorage,
-    _sessionStorage,
-    //_indexedDB,
-];
+var _tests = [{
+    id: "localStorage",
+    isSupported: !!window.localStorage,
+    init: function() { window.localStorage.setItem(_TEST_KEY, _TEST_VALUE); },
+    isFailed: function() { return window.localStorage.getItem(_TEST_KEY) !== null; },
+}, {
+
+    id: "sessionStorage",
+    isSupported: !!window.sessionStorage,
+    init: function() { window.sessionStorage.setItem(_TEST_KEY, _TEST_VALUE); },
+    isFailed: function() { return window.sessionStorage.getItem(_TEST_KEY) !== null; },
+}, {
+
+    id: 'indexedDB',
+    isSupported: !!window.indexedDB,
+
+    _open: function() { return window.indexedDB.open('PB', 1); },
+    //.onsuccess .onerror .onupgradeneeded
+    init: function() { },
+    isFailed: function() { return true; },
+}];
 
 window.onload = function() {
     for (var i = 0; i < _tests.length; i++) {
