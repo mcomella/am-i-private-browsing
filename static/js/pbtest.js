8dacd52579bcb0c3422252c34a84016b2b609c39
Michael Comella <michael.l.comella@gmail.com>
2017-12-21 20:04:22 -0800

    Add local storage test.

--- static/js/pbtest.js
+++ static/js/pbtest.js
@@ -1,9 +1,23 @@
-var isLocalStorageSupported = !!window.localStorage;
+var _TEST_KEY = 'am-i-private-browsing';
+var _TEST_VALUE = 'apparently not';
 
-function testLocalStorage() {
-    alert('local storage!');
+var _pbLocalStorage = {
+    isSupported: !!window.localStorage,
+    init: function() { window.localStorage.setItem(_TEST_KEY, _TEST_VALUE); },
+    hasOldData: function() { return window.localStorage.getItem(_TEST_KEY) !== null; },
+};
+
+if (_pbLocalStorage.isSupported) {
+    if (!_pbLocalStorage.hasOldData()) {
+        alert('pass!');
+    } else {
+        alert('fail!');
+    }
+
+    _pbLocalStorage.init();
 }
 
-if (isLocalStorageSupported) {
-    testLocalStorage();
-}
+// feature name, feature test
+// if test does not pass, modify feature class
+//
+// initialize new test.
