03ac0fd92e72e5b99f41ee63300f202ca5ef9e75
Michael Comella <michael.l.comella@gmail.com>
2017-12-21 19:29:47 -0800

    Init app as ktor

--- src/main/kotlin/org/mozilla/pbtest/App.kt
+++ src/main/kotlin/org/mozilla/pbtest/App.kt
@@ -1,5 +1,22 @@
 package org.mozilla.pbtest
 
-fun main(args: Array<String>) {
-    System.out.println("lol")
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+
+fun Application.main() {
+    installFeatures()
+    routing {
+        get("/") {}
+    }
+}
+
+private fun Application.installFeatures() {
+    install(DefaultHeaders)
+    install(CallLogging)
 }
