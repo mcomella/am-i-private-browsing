5c5aac527391ed9118e02dc3365b863d25221875
Michael Comella <michael.l.comella@gmail.com>
2017-12-21 20:16:47 -0800

    Style page on local storage test status.

--- static/js/pbtest.js
+++ static/js/pbtest.js
@@ -1,20 +1,34 @@
 var _TEST_KEY = 'am-i-private-browsing';
 var _TEST_VALUE = 'apparently not';
 
+var _CLASS_PASS = "pass";
+var _CLASS_FAIL = "fail";
+
 var _pbLocalStorage = {
+    id: "localStorage",
     isSupported: !!window.localStorage,
     init: function() { window.localStorage.setItem(_TEST_KEY, _TEST_VALUE); },
     hasOldData: function() { return window.localStorage.getItem(_TEST_KEY) !== null; },
 };
 
-if (_pbLocalStorage.isSupported) {
-    if (!_pbLocalStorage.hasOldData()) {
-        alert('pass!');
-    } else {
-        alert('fail!');
-    }
+function updatePass(id) {
+    document.getElementById(id).className = _CLASS_PASS;
+}
 
-    _pbLocalStorage.init();
+function updateFail(id) {
+    document.getElementById(id).className = _CLASS_FAIL;
+}
+
+window.onload = function() {
+    if (_pbLocalStorage.isSupported) {
+        if (!_pbLocalStorage.hasOldData()) {
+            updatePass(_pbLocalStorage.id);
+        } else {
+            updateFail(_pbLocalStorage.id);
+        }
+
+        _pbLocalStorage.init();
+    }
 }
 
 // feature name, feature test
